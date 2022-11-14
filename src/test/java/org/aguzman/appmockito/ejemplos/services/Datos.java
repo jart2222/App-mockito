128689ee923159a05a10d231181867d76fef75d5
jart2222 <jart2607@gmail.com>
2022-11-13 20:43:52 -0600

    Initial commit

---
+++ src/test/java/org/aguzman/appmockito/ejemplos/services/Datos.java
@@ -0,0 +1,17 @@
+package org.aguzman.appmockito.ejemplos.services;
+
+import org.aguzman.appmockito.ejemplos.models.Examen;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Datos {
+    public final static List<Examen> EXAMENES= Arrays.asList(new Examen(5L, "Matematicas"), new Examen(6L, "Lenguaje"),
+            new Examen(7L, "Historia"));
+
+    public final static List<String> PREGUNTAS=Arrays.asList("arimética", "integrales",
+            "derivadas", "trigonometria", "geometria");
+
+    public final static Examen EXAMEN=new Examen(null, "Física");
+
+}
